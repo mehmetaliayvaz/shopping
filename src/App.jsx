4dd0fd7e8f88bfaf6bc3e7790c82074f3f774437
Mehmet Ali Ayvaz <mehmetaliayvaz61@gmail.com>
2022-11-21 16:53:13 +0300

    install react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import TheHeader from "./components/TheHeader";
 import HomePage from "./pages/HomePage";
+import ProductDetailPage from "./pages/ProductDetailPage";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <div className="bg-gray-50">
       <TheHeader />
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product-detail" element={<ProductDetailPage />} />
+      </Routes>
     </div>
   );
 }
