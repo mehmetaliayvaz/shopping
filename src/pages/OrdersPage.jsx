33468a5689f29502f055fc2b2eb67593af8a6f04
Mehmet Ali Ayvaz <mehmetaliayvaz61@gmail.com>
2022-11-29 03:06:04 +0300

    orders-page add twarning

--- src/pages/OrdersPage.jsx
+++ src/pages/OrdersPage.jsx
@@ -6,27 +6,32 @@ function OrdersPage() {
   return (
     <div className="page-wrapper">
       <div className="container">
-        <div>
-          {
-            orders.map((orderItem, orderIndex) => {
-              return(
-                <div key={orderIndex} className="my-20">
-                  <h2 className="text-xl font-semibold mb-5 text-primary">
-                    Sipariş ID: 
-                    <span className=""> { orderIndex + 1 }</span>
-                  </h2>
-                  {
-                    orderItem.map((productItem, productIndex) => {
-                      return(
-                        <ShoppingCartProduct key={productIndex} product={productItem} />
-                      )
-                    })
-                  }
-                </div>
-              )
-            })
-          }
-        </div>
+        {
+          orders.length > 0 ? (
+          <div>
+            {
+              orders.map((orderItem, orderIndex) => {
+                return(
+                  <div key={orderIndex} className="my-20">
+                    <h2 className="text-xl font-semibold mb-5 text-primary">
+                      Sipariş ID: 
+                      <span className=""> { orderIndex + 1 }</span>
+                    </h2>
+                    {
+                      orderItem.map((productItem, productIndex) => {
+                        return(
+                          <ShoppingCartProduct key={productIndex} product={productItem} />
+                        )
+                      })
+                    }
+                  </div>
+                )
+              })
+            }
+          </div> ) : (
+            <h1 className="text-3xl font-semibold py-20 text-center">Geçmiş siparişiniz bulunmamaktadır...</h1>
+          )
+        }
       </div>
     </div>
   )
