a08335f2a5b52cc967cfe2de1c717620620004c9
Mehmet Ali Ayvaz <mehmetaliayvaz61@gmail.com>
2022-11-29 03:03:24 +0300

    orders-page edited

--- src/pages/OrdersPage.jsx
+++ src/pages/OrdersPage.jsx
@@ -1,9 +1,33 @@
-// import ShoppingCartProduct from '../components/ShoppingCartProduct';
+import ShoppingCartProduct from '../components/ShoppingCartProduct';
+import { useSelector} from 'react-redux';
 
 function OrdersPage() {
+  const orders = useSelector(state => state.orders.value);
   return (
     <div className="page-wrapper">
-      {/* <ShoppingCartProduct /> */}
+      <div className="container">
+        <div>
+          {
+            orders.map((orderItem, orderIndex) => {
+              return(
+                <div key={orderIndex} className="my-20">
+                  <h2 className="text-xl font-semibold mb-5 text-primary">
+                    Sipariş ID: 
+                    <span className=""> { orderIndex + 1 }</span>
+                  </h2>
+                  {
+                    orderItem.map((productItem, productIndex) => {
+                      return(
+                        <ShoppingCartProduct key={productIndex} product={productItem} />
+                      )
+                    })
+                  }
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
     </div>
   )
 }
